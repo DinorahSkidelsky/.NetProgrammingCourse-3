6f67a5f8376af9ec0e77b9a7dbf508f392142cba
Dinorah <dinorahskidelsky@gmail.com>
2022-10-26 12:20:31 +0200

    Add project files.

---
+++ Ejercicio/Persona.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    class Persona
+    {
+        private string nombre;
+        private string email;
+        private int sueldo;
+        private string sexo;
+        private int peso;
+        private double altura;
+
+        public Persona(string nombre, int sueldo, string sexo, int peso, double altura)
+        {
+            this.nombre = nombre;
+            this.sexo = sexo;
+            this.sueldo = sueldo;
+            this.peso = peso;
+            this.altura = altura;
+            CrearEmail();
+        }
+
+        private void CrearEmail()
+        {
+            email = nombre + "@superempresa.com";
+        }
+
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public string GetSueldo()
+        {
+            return sueldo.ToString();
+        }
+
+        public string GetSexo()
+        {
+            return sexo;
+        }
+
+        public string GetPeso()
+        {
+            return peso.ToString();
+        }
+
+        public string GetAltura()
+        {
+            return altura.ToString();
+        }
+
+    }
+}
---
+++ Ejercicio/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int sueldo;
+            string sexo;
+            int peso;
+            double altura;
+
+            Console.WriteLine("Bienvenidos al sistema de gestión de personas, a continuación ingrese sus datos:");
+            Console.WriteLine("Ingrese nombre:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese sueldo:");
+            sueldo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese sexo:");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese peso:");
+            peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese altura:");
+            altura = double.Parse(Console.ReadLine());
+
+            Persona primeraPersona = new Persona(nombre, sueldo, sexo, peso, altura);
+            Console.WriteLine("");
+            Console.WriteLine("Datos de la persona registrada: ");
+            Console.WriteLine("-Nombre: " + primeraPersona.GetNombre());
+            Console.WriteLine("-Email: " + primeraPersona.GetEmail());
+            Console.WriteLine("-Sueldo: " + primeraPersona.GetSueldo() + " ARS");
+            Console.WriteLine("-Sexo: " + primeraPersona.GetSexo());
+            Console.WriteLine("-Peso: " + primeraPersona.GetPeso() + " kg(B)");
+            Console.WriteLine("-Altura: " + primeraPersona.GetAltura() + " m");
+            Console.Read();
+        }
+    }
+}
